5bbd5f080d5a7a92bf1c0ae9d10af43fcf5e1702
Anderson <anderlentz@gmail.com>
2020-01-29 17:01:20 -0300

    Pass email and password remote's auth request body

--- EmpresasApp/EmpresasAppTests/Authentication Tests/RemoteAuthServiceTests.swift
+++ EmpresasApp/EmpresasAppTests/Authentication Tests/RemoteAuthServiceTests.swift
@@ -13,13 +13,25 @@ class RemoteAuthService {
     let client: HTTPClient
     let endpointURL: URL
     
+    var headers: [String: String] = ["Content-Type":"application/json"]
+    
+    var body: [String:String] = ["email":"email_test@email.com",
+                                 "password":"123"]
+    
+    
     init(endpointURL: URL, client: HTTPClient) {
         self.client = client
         self.endpointURL = endpointURL
     }
     
     func authenticate(email: String, password: String) {
+        self.setupHeaders(email: email, password: password)
         client.post(to: endpointURL)
+    }
+    
+    private func setupHeaders(email: String, password: String) {
+        self.body["email"] = email
+        self.body["password"] = password
     }
 }
 
@@ -46,7 +58,17 @@ class RemoteAuthServiceTests: XCTestCase {
         XCTAssertEqual(client.endpointURL,endpointURL)
     }
     
-    
+    func test_authenticate_withEmailPasswordIntoBody() {
+        let (sut,_) = makeSUT()
+        let email = "email@email.com"
+        let password = "123123"
+        
+        sut.authenticate(email: email, password: password)
+        
+        XCTAssertEqual(sut.body,["email": email,"password":password])
+        
+        
+    }
    
     
     // MARK: - Helpers
