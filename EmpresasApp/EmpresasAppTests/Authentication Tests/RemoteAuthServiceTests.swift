d602388dc0e7710f16ac92a1fee8a3594ee62530
Anderson Lentz <anderlent@gmail.com>
2020-01-29 15:59:47 -0300

    Make HTTPClient a protocol

--- EmpresasApp/EmpresasAppTests/Authentication Tests/RemoteAuthServiceTests.swift
+++ EmpresasApp/EmpresasAppTests/Authentication Tests/RemoteAuthServiceTests.swift
@@ -9,23 +9,49 @@
 import XCTest
 
 class RemoteAuthService {
- 
+
+    let client: HTTPClient
+    
+    func authenticate() {
+        client.post(to: URL(string: "https://test-authentication.com")!)
+    }
+    
+    init(client: HTTPClient) {
+        self.client = client
+    }
 }
 
-class HTTPClient {
+protocol HTTPClient {
+    func post(to url: URL)
+}
+
+class HTTPClientSpy: HTTPClient {
+    
     var endpointURL: URL?
+    
+    func post(to url: URL) {
+        self.endpointURL = url
+    }
 }
-
 
 class RemoteAuthServiceTests: XCTestCase {
     
     func test_init_doesNotRequestAuthenticationDataFromEndpoint() {
-        let client = HTTPClient()
-        _ = RemoteAuthService()
-        
+        let client = HTTPClientSpy()
+        _ = RemoteAuthService(client: client)
         
         XCTAssertNil(client.endpointURL)
-        
     }
+    
+    func test_authenticate_requestDataFromEndpointURL() {
+        let client = HTTPClientSpy()
+        let sut = RemoteAuthService(client: client)
+
+        sut.authenticate()
+
+        XCTAssertNotNil(client.endpointURL)
+    }
+   
+    
 }
 
