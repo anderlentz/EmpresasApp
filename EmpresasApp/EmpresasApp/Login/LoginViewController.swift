8912b2273639d1dc60168b143d1630f2f3fdbfe9
Anderson <anderlentz@gmail.com>
2020-01-30 12:46:36 -0300

    Implements LoginViewController with storyboard layout

--- EmpresasApp/EmpresasApp/Login/LoginViewController.swift
+++ EmpresasApp/EmpresasApp/Login/LoginViewController.swift
@@ -9,22 +9,17 @@
 import UIKit
 
 class LoginViewController: UIViewController {
-
-    override func viewDidLoad() {
-        super.viewDidLoad()
-
-        // Do any additional setup after loading the view.
+    
+    // MARK: - Outlets
+    @IBOutlet weak var emailTextField: UITextField!
+    @IBOutlet weak var passwordTextField: UITextField!
+    
+    // MARK: - IBActions
+    @IBAction func loginButtonAction(_ sender: UIButton) {
     }
     
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
+    // MARK: - Lifecycle methods
+    override func viewDidLoad() {
+        super.viewDidLoad()
     }
-    */
-
 }
