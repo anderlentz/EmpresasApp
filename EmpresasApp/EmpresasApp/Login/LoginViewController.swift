346ddff7d8d6168ac082634eecfc6704df57ab98
Anderson <anderlentz@gmail.com>
2020-01-30 12:05:48 -0300

    Add logo assets and loginViewController

---
+++ EmpresasApp/EmpresasApp/Login/LoginViewController.swift
@@ -0,0 +1,30 @@
+//
+//  LoginViewController.swift
+//  EmpresasApp
+//
+//  Created by Anderson on 30/01/20.
+//  Copyright © 2020 Anderson. All rights reserved.
+//
+
+import UIKit
+
+class LoginViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
