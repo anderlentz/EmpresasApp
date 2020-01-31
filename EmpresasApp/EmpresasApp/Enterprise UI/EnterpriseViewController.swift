e25667d9137fb59d44eb93df3d26608fc682b7dc
Anderson <anderlentz@gmail.com>
2020-01-30 21:45:16 -0300

    Add enterprise storyboard and view controller and adjust enterprise cell

---
+++ EmpresasApp/EmpresasApp/Enterprise UI/EnterpriseViewController.swift
@@ -0,0 +1,16 @@
+//
+//  EnterpriseViewController.swift
+//  EmpresasApp
+//
+//  Created by Anderson on 30/01/20.
+//  Copyright © 2020 Anderson. All rights reserved.
+//
+
+import UIKit
+
+class EnterpriseViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+}
