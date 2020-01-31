91534900f455568f74c626fe4a983a10dfc0e142
Anderson <anderlentz@gmail.com>
2020-01-31 02:26:07 -0300

    Make URLProtocolStub intercept all URL requests

--- EmpresasApp/EmpresasAppTests/Authentication Tests/URLSessionHTTPClientTests.swift
+++ EmpresasApp/EmpresasAppTests/Authentication Tests/URLSessionHTTPClientTests.swift
@@ -16,7 +16,8 @@ class URLSessionHTTPClient {
     }
     
     func post(to url: URL, completion: @escaping (Result<(Data, HTTPURLResponse), Error>) -> Void) {
-        let urlRequest = URLRequest(url: url)
+        var urlRequest = URLRequest(url: url)
+        urlRequest.httpMethod = "POST"
         session.dataTask(with: urlRequest) { (_, _, error) in
             if let error = error {
                 completion(.failure(error))
@@ -29,9 +30,9 @@ class URLSessionHTTPClientTests: XCTestCase {
     
     func test_postUrlRequest_failsOnRequestError() {
         URLProtocolStub.startInterceptingRequests()
-        let endpointURL = URL(string: "https://test-authentication.com")!
+        let endpointURL = URL(string: "https://auth.test.com")!
         let error = NSError(domain: "ANY ERROR", code: 1)
-        URLProtocolStub.stub(url: endpointURL, data: nil, response: nil, error: error)
+        URLProtocolStub.stub(data: nil, response: nil, error: error)
         
         let sut = URLSessionHTTPClient()
         
@@ -51,23 +52,21 @@ class URLSessionHTTPClientTests: XCTestCase {
         URLProtocolStub.stopInterceptingRequests()
     }
     
+    // MARK: - URLProtocolStub class
+    
     private class URLProtocolStub: URLProtocol {
-        private static var stubs = [URL: Stub]()
-        
+        private static var stub: Stub?
+        private static var requestObserver: ( (URLRequest) -> Void)?
         private struct Stub {
             let data: Data?
             let response: URLResponse?
             let error: Error?
         }
-        
-        static func stub(url: URL, data: Data?, response: URLResponse?, error: Error?) {
-            stubs[url] = Stub(data: data, response: response, error: error)
-        }
-        
+
         override class func canInit(with request: URLRequest) -> Bool {
-            guard let url = request.url else { return false}
-            
-            return URLProtocolStub.stubs[url] != nil
+            //Intercep all requests
+            requestObserver?(request)
+            return true
         }
         
         override class func canonicalRequest(for request: URLRequest) -> URLRequest {
@@ -75,9 +74,16 @@ class URLSessionHTTPClientTests: XCTestCase {
         }
         
         override func startLoading() {
-            guard let url = request.url, let stub = URLProtocolStub.stubs[url] else { return }
             
-            if let error = stub.error {
+            if let data = URLProtocolStub.stub?.data {
+                client?.urlProtocol(self, didLoad: data)
+            }
+            
+            if let response = URLProtocolStub.stub?.response {
+                client?.urlProtocol(self, didReceive: response, cacheStoragePolicy: .notAllowed)
+            }
+            
+            if let error = URLProtocolStub.stub?.error {
                 client?.urlProtocol(self, didFailWithError: error)
             }
             client?.urlProtocolDidFinishLoading(self)
@@ -85,13 +91,22 @@ class URLSessionHTTPClientTests: XCTestCase {
         
         override func stopLoading() {}
         
+        static func stub(data: Data?, response: URLResponse?, error: Error?) {
+            stub = Stub(data: data, response: response, error: error)
+        }
+        
         static func startInterceptingRequests() {
             URLProtocol.registerClass(URLProtocolStub.self)
         }
         
         static func stopInterceptingRequests() {
             URLProtocol.unregisterClass(URLProtocolStub.self)
-            stubs = [:]
+            stub = nil
+            requestObserver = nil
+        }
+        
+        static func observeRquests(observer: @escaping (URLRequest) -> Void) {
+            requestObserver = observer
         }
     }
 }
