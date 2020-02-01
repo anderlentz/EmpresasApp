b42de88121b81596f48495438b7438951f86a5f6
Anderson <anderlentz@gmail.com>
2020-02-01 20:54:42 -0300

    Implement EnterpriseHTTPClient using URLSession

--- EmpresasApp/EmpresasApp/Enterprise API/URLSessionEnterpriseHTTPClient.swift
+++ EmpresasApp/EmpresasApp/Enterprise API/URLSessionEnterpriseHTTPClient.swift
@@ -11,3 +11,29 @@ import Foundation
 protocol EnterpriseHTTPClient {
     func get(from urlRequest: URLRequest, completion: @escaping (Result<(Data,HTTPURLResponse),Error>) -> Void)
 }
+
+class URLSessionEnterpriseHTTPCLient: EnterpriseHTTPClient {
+    
+    private let session: URLSession
+    
+    init(session: URLSession = .shared) {
+        self.session = session
+    }
+    
+    private struct UnexpectedValues: Error {}
+    
+    func get(from urlRequest: URLRequest, completion: @escaping (Result<(Data, HTTPURLResponse), Error>) -> Void) {
+        
+        session.dataTask(with: urlRequest) { (data, response, error) in
+            if let error = error {
+                completion(.failure(error))
+            } else if let data = data, let response = response as? HTTPURLResponse {
+                completion(.success((data, response)))
+            }
+            else {
+                completion(.failure(UnexpectedValues()))
+                //completion(.failure(s))
+            }
+        }.resume()
+    }
+}
