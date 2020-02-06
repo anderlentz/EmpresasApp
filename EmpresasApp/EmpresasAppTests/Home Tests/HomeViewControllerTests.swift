ff0959769a21ad1d68fe64af6d2983772956529b
Anderson <anderlentz@gmail.com>
2020-02-06 14:15:54 -0300

    OnLoadEnterprises is invoked when get enterprises completes with success

--- EmpresasApp/EmpresasAppTests/Home Tests/HomeViewControllerTests.swift
+++ EmpresasApp/EmpresasAppTests/Home Tests/HomeViewControllerTests.swift
@@ -23,6 +23,24 @@ class HomeViewControllerTests: XCTestCase {
     
     }
     
+    func test_searchEnterprises_callOnEnterprisesLoad() {
+        let viewModel = HomeViewModelSpy()
+        let sut = HomeUIComposer.loginComposedWith(viewModel: viewModel)
+        var receivedEnterprises: [Enterprise]?
+        sut.loadViewIfNeeded()
+        
+        let exp = expectation(description: "Wait on enterprises load")
+        sut.viewModel?.onEnterprisesLoad = { enterprises in
+            receivedEnterprises = enterprises
+            exp.fulfill()
+        }
+        viewModel.completesGetAllEnterprisesWithSuccess()
+        
+        wait(for: [exp], timeout: 1.0)
+        XCTAssertEqual(receivedEnterprises, [HomeViewControllerTests.makeEnterprise()])
+        
+    }
+    
     private class HomeViewModelSpy: HomeViewModelProtocol {
         
         var wasGetAllEnterprisesCalled = false
@@ -33,10 +51,22 @@ class HomeViewControllerTests: XCTestCase {
         
         func getAllEnterprises(enterpriseName: String) {
             wasGetAllEnterprisesCalled = true
+            
         }
         
-    
-        
+        func completesGetAllEnterprisesWithSuccess() {
+            onEnterprisesLoad?([HomeViewControllerTests.makeEnterprise()])
+        }
     }
     
+    static private func makeEnterprise() -> Enterprise {
+        return Enterprise(id: 0, emailEnterprise: nil, facebook: nil, twitter: nil, linkedin: nil, phone: nil, ownEnterprise: false, enterpriseName: "Test", photo: nil, enterprisDescription: "Test", city: "City", country: "Country", value: 0, sharePrice: 1, enterpriseType: EnterpriseType(id: 0, enterpriseTypeName: "Enterprise"))
+    }
+}
+
+private extension HomeViewController {
+    func simulateSuccefullEnterpriseSearch(){
+        viewModel?.getAllEnterprises(enterpriseName: "teste")
+        
+    }
 }
